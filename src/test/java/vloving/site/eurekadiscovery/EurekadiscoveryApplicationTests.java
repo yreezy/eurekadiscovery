bcc282e51bb7ed0ca32fc94a93c0b0dfbc31b63b
yrezzy <yrezzy@foxmail.com>
2021-03-08 18:29:11 +0800

    eureka 服务发现

---
+++ src/test/java/vloving/site/eurekadiscovery/EurekadiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package vloving.site.eurekadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
