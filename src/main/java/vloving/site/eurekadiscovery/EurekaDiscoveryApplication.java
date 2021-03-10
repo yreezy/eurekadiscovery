0085364dbff5dd59d59bb30ceabf3a4826f3d958
yrezzy <196218030@qq.com>
2021-03-10 16:21:55 +0800

    eureka 服务发现端口修改

--- src/main/java/vloving/site/eurekadiscovery/EurekaDiscoveryApplication.java
+++ src/main/java/vloving/site/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekadiscoveryApplication {
+public class EurekaDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekadiscoveryApplication.class, args);
+        SpringApplication.run(EurekaDiscoveryApplication.class, args);
     }
 
 }
--- src/test/java/vloving/site/eurekadiscovery/EurekaDiscoveryApplicationTests.java
+++ src/test/java/vloving/site/eurekadiscovery/EurekaDiscoveryApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekadiscoveryApplicationTests {
+class EurekaDiscoveryApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
